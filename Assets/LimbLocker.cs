b0adde6dae80311d7b61455803fd3f9eb6faaa72
Gulyás János Adrián <e81to9@inf.elte.hu>
2023-09-01 23:25:13 +0200

    visualizers now change color depending on whether the player's landmark is in the correct position or not, update function only runs when there is a lock

--- Assets/LimbLocker.cs
+++ Assets/LimbLocker.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class LimbLocker : MonoBehaviour
 {
 
+    public Material materialCorrect; //Material to use when the limb is in the correct position
+    public Material materialIncorrect; //Material to use when the limb is in an incorrect position
+
     public float ScaleFactor = 0.1f;
     public float Threshold = 0.5f; //maximum distance between the limb and the lock position
     public GameObject warningCanvas;
     private Pose.Landmark lockedLimb;
+    private int lockedLimbIndex = 16; //TODO: it would be good to be able to find out the index in the .landmarks data from knowing the lockedLimb
     private Vector3 lockedLimbPosition; //where must the locked limb stay
     public GameObject[] lockVisualizationObjects = new GameObject[2];
     public Vector3 playerBasePosition = new Vector3(-4, 0.801f, 0);
     public Vector3 mirrorBasePosition = new Vector3(12.46f, 5, -1.73f);
     public EstimationToIK estimationScript;
 
-
+    private bool locked = false;
     private float warningTimer = 0f;
     // Start is called before the first frame update
     void Start()
@@ -23,25 +27,42 @@ public class LimbLocker : MonoBehaviour
         
     }
 
-    //TODO: it would be good to be able to find out the index in the .landmarks data from knowing the lockedLimb
+    void turnColor(bool correct)
+    {
+        foreach (GameObject visualizer in lockVisualizationObjects)
+        {
+            Renderer render = visualizer.GetComponent<Renderer>();
+            if (correct)
+            {
+                render.material = materialCorrect;
+            }
+            else
+            {
+                render.material = materialIncorrect;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (estimationScript)
+        if (estimationScript && locked)
         {
-            if(estimationScript.landmarks.Length > 0)
+            if(estimationScript.landmarks.Length > lockedLimbIndex)
             {
-                if (Vector3.Distance(estimationScript.landmarks[0], lockedLimbPosition) > Threshold) 
+                if (Vector3.Distance(estimationScript.landmarks[lockedLimbIndex], lockedLimbPosition) > Threshold) 
                 {
                     warningTimer += Time.deltaTime;
                     if (warningTimer >= 1f)
                     {
                         warningCanvas.SetActive(true);
+                        turnColor(false);
                     }
                 }
                 else
                 {
                     warningCanvas.SetActive(false);
+                    turnColor(true);
                     warningTimer = 0f;
                 }
 
@@ -51,6 +72,7 @@ public class LimbLocker : MonoBehaviour
 
     public void lockLimb(Gesture goalGesture, Gesture lastGoalGesture)
     {
+        locked = true;
         List<Pose.Landmark> landmarkList = goalGesture.relatedLandmarks();
         int randomIndex = new System.Random().Next(landmarkList.Count);
         lockedLimb = landmarkList[randomIndex];
@@ -64,7 +86,9 @@ public class LimbLocker : MonoBehaviour
 
     public void releaseLockedLimb()
     {
+        locked = false;
         lockVisualizationObjects[0].SetActive(false);
         lockVisualizationObjects[1].SetActive(false);
+        warningCanvas.SetActive(false);
     }
 }
