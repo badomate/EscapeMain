db6fd8ac47850297cdfad94f4239bfb95ef96371
Gulyás János Adrián <e81to9@inf.elte.hu>
2023-12-07 18:33:07 +0100

    iterates through the data and creates a gesture
    Former-commit-id: 605b899555e95c7a5c2c82e0b56072abc25d0eb2

--- Assets/readNpy.cs
+++ Assets/readNpy.cs
@@ -16,8 +16,27 @@ public class readNpy : MonoBehaviour
 
         // Access and manipulate the data as needed
         // For example, print the shape of the array
-        Debug.Log("Shape of the array: " + npArray.shape);
-        Debug.Log(npArray[0]);
-    
+        //Debug.Log("Shape of the array: " + npArray.shape);
+        //Debug.Log(npArray[0]);
+
+        List<Pose> poseList = new List<Pose>();
+
+        for (int i = 0; i < 100; i++) //TODO: want npArray.shape[0] instead but gives errors
+        {
+            Pose currentPose = new Pose();
+            foreach (var landmark in LandmarkIndicesDictionary.cocoIndices.Keys)
+            {
+                int landmarkIndex = LandmarkIndicesDictionary.cocoIndices[landmark];
+                float x = npArray[i, landmarkIndex, 0];
+                float y = npArray[i, landmarkIndex, 1];
+                float z = npArray[i, landmarkIndex, 2];
+                Vector3 landmarkPosition = new Vector3(x, y, z);
+
+                currentPose._landmarkArrangement.Add(landmark, landmarkPosition);
+            }
+            poseList.Add(currentPose);
+        }
+        Gesture endResult = new Gesture();
+        endResult.AddPoses(poseList);
     }
 }
