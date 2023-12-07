c1553e5e75db0d2d4d712a21358e7c98758238f5
Gulyás János Adrián <e81to9@inf.elte.hu>
2023-12-07 18:42:12 +0100

    fixes the index out of range error
    Former-commit-id: e9de3a60698e45575004cf0bcd457d072dfd1ab2

--- Assets/readNpy.cs
+++ Assets/readNpy.cs
@@ -6,6 +6,7 @@ using NumSharp;
 
 public class readNpy : MonoBehaviour
 {
+    public RiggingIK rigToUse;
     void Start()
     {
         // Specify the path to your .npy file
@@ -21,15 +22,15 @@ public class readNpy : MonoBehaviour
 
         List<Pose> poseList = new List<Pose>();
 
-        for (int i = 0; i < 100; i++) //TODO: want npArray.shape[0] instead but gives errors
+        for (int i = 0; i < npArray.shape[0]; i++) //TODO: want npArray.shape[0] instead but gives errors
         {
             Pose currentPose = new Pose();
             foreach (var landmark in LandmarkIndicesDictionary.cocoIndices.Keys)
             {
                 int landmarkIndex = LandmarkIndicesDictionary.cocoIndices[landmark];
-                float x = npArray[i, landmarkIndex, 0];
-                float y = npArray[i, landmarkIndex, 1];
-                float z = npArray[i, landmarkIndex, 2];
+                float x = npArray[i, 0, landmarkIndex];
+                float y = npArray[i, 1, landmarkIndex];
+                float z = npArray[i, 2, landmarkIndex];
                 Vector3 landmarkPosition = new Vector3(x, y, z);
 
                 currentPose._landmarkArrangement.Add(landmark, landmarkPosition);
@@ -38,5 +39,7 @@ public class readNpy : MonoBehaviour
         }
         Gesture endResult = new Gesture();
         endResult.AddPoses(poseList);
+
+        rigToUse.playGesture(endResult);
     }
 }
