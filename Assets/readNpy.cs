45411ff1a2533e094b9e9cbd9d90df1567252307
Gulyás János Adrián <e81to9@inf.elte.hu>
2023-12-07 18:46:09 +0100

    turns off mirroring to not intervene with playGesture
    Former-commit-id: 83f7c2f5e1fbac7eaae2387c0ca4df0af02745d6

--- Assets/readNpy.cs
+++ Assets/readNpy.cs
@@ -9,16 +9,8 @@ public class readNpy : MonoBehaviour
     public RiggingIK rigToUse;
     void Start()
     {
-        // Specify the path to your .npy file
         string filePath = "Assets/GestureDictionary/Recordings/points_3d_list.npy";
-
-        // Read the .npy file
         NDArray npArray = np.load(filePath);
-
-        // Access and manipulate the data as needed
-        // For example, print the shape of the array
-        //Debug.Log("Shape of the array: " + npArray.shape);
-        //Debug.Log(npArray[0]);
 
         List<Pose> poseList = new List<Pose>();
 
@@ -40,6 +32,7 @@ public class readNpy : MonoBehaviour
         Gesture endResult = new Gesture();
         endResult.AddPoses(poseList);
 
+        Debug.Log(endResult._poseSequence.Count);
         rigToUse.playGesture(endResult);
     }
 }
