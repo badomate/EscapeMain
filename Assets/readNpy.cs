19b0594310c4f50d3c68981411dd2176720b7893
Gulyás János Adrián <e81to9@inf.elte.hu>
2023-12-07 19:35:41 +0100

    rounds the elements
    Former-commit-id: 560fd3586a41666c44246805fed009ae6cba2050

--- Assets/readNpy.cs
+++ Assets/readNpy.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using NumSharp;
 using UnityEngine.Experimental.AI;
+using System.Linq;
 
 public class readNpy : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class readNpy : MonoBehaviour
     public static Gesture endResult;
 
     bool playing = true;
+
     void Update()
     {
         if (playing) //bit of a workaround to make sure everything else is already initialized and this only plays once
@@ -26,11 +28,15 @@ public class readNpy : MonoBehaviour
                 Pose currentPose = new Pose();
                 foreach (var landmark in LandmarkIndicesDictionary.cocoIndices.Keys)
                 {
-                    int worldScale = 100;
                     int landmarkIndex = LandmarkIndicesDictionary.cocoIndices[landmark];
-                    float x = npArray[i, 0, landmarkIndex] / worldScale;
-                    float y = npArray[i, 1, landmarkIndex] / worldScale;
-                    float z = npArray[i, 2, landmarkIndex] / worldScale;
+                    float x = npArray[i, 0, landmarkIndex];
+                    float y = npArray[i, 1, landmarkIndex];
+                    float z = npArray[i, 2, landmarkIndex];
+
+                    x = Mathf.Round(x * 1000f) / 1000f;
+                    y = Mathf.Round(y * 1000f) / 1000f;
+                    z = Mathf.Round(z * 1000f) / 1000f;
+
                     Vector3 landmarkPosition = new Vector3(x, y, z);
 
                     currentPose._landmarkArrangement.Add(landmark, landmarkPosition);
