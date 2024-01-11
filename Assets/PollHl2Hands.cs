3583fd0ab6fc34208ea6461a0b82b127e87569f0
Gulyás János Adrián <e81to9@inf.elte.hu>
2024-01-11 16:31:40 +0100

    adds hand polling
    Former-commit-id: 0e646c03400ba4ccc32d3d5e7e300215b4ec3963

---
+++ Assets/PollHl2Hands.cs
@@ -0,0 +1,59 @@
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalHandListenerExample : MonoBehaviour,
+    IMixedRealitySourceStateHandler, // Handle source detected and lost
+    IMixedRealityHandJointHandler // handle joint position updates for hands
+{
+    private void OnEnable()
+    {
+        // Instruct Input System that we would like to receive all input events of type
+        // IMixedRealitySourceStateHandler and IMixedRealityHandJointHandler
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealitySourceStateHandler>(this);
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealityHandJointHandler>(this);
+    }
+
+    private void OnDisable()
+    {
+        // This component is being destroyed
+        // Instruct the Input System to disregard us for input event handling
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealitySourceStateHandler>(this);
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityHandJointHandler>(this);
+    }
+
+    // IMixedRealitySourceStateHandler interface
+    public void OnSourceDetected(SourceStateEventData eventData)
+    {
+        var hand = eventData.Controller as IMixedRealityHand;
+
+        // Only react to articulated hand input sources
+        if (hand != null)
+        {
+            Debug.Log("Source detected: " + hand.ControllerHandedness);
+        }
+    }
+
+    public void OnSourceLost(SourceStateEventData eventData)
+    {
+        var hand = eventData.Controller as IMixedRealityHand;
+
+        // Only react to articulated hand input sources
+        if (hand != null)
+        {
+            Debug.Log("Source lost: " + hand.ControllerHandedness);
+        }
+    }
+
+    public void OnHandJointsUpdated(
+                InputEventData<IDictionary<TrackedHandJoint, MixedRealityPose>> eventData)
+    {
+        MixedRealityPose palmPose;
+        if (eventData.InputData.TryGetValue(TrackedHandJoint.Palm, out palmPose))
+        {
+            Debug.Log("Hand Joint Palm Updated: " + palmPose.Position);
+        }
+    }
+}
