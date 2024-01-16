8a35c41f2b9db4b0a3a4f0ffce3a01d46f8fd2fe
Gulyás János Adrián <e81to9@inf.elte.hu>
2024-01-16 15:14:23 +0100

    convers xr hand joints to pose
    Former-commit-id: cd9f1cc87298c17303f4f4be367447e02f31a66c

--- Assets/PollHl2Hands.cs
+++ Assets/PollHl2Hands.cs
@@ -1,18 +1,39 @@
-//using Microsoft.MixedReality.Toolkit.Input;
-//using Microsoft.MixedReality.Toolkit.Utilities;
-using Microsoft.MixedReality.Toolkit;
 using MixedReality.Toolkit.Subsystems;
 using MixedReality.Toolkit;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-using System.Collections;
 using System;
-//using Microsoft.MixedReality.OpenXR;
 
 public class PollHl2Hands : MonoBehaviour
 {
     private HandsAggregatorSubsystem subsystem;
+
+    Dictionary<Pose.Landmark, Vector3> poseDictionary;
+
+    Dictionary<TrackedHandJoint, Pose.Landmark> jointToLandmarkMapping = new Dictionary<TrackedHandJoint, Pose.Landmark>()
+{
+    { TrackedHandJoint.IndexTip, Pose.Landmark.LEFT_INDEX },
+    { TrackedHandJoint.IndexMetacarpal, Pose.Landmark.LEFT_INDEX_BASE },
+    { TrackedHandJoint.IndexProximal, Pose.Landmark.LEFT_INDEX_KNUCKLE },
+
+    { TrackedHandJoint.LittleTip, Pose.Landmark.LEFT_PINKY },
+    { TrackedHandJoint.LittleMetacarpal, Pose.Landmark.LEFT_PINKY_BASE },
+    { TrackedHandJoint.LittleProximal, Pose.Landmark.LEFT_PINKY_KNUCKLE },
+
+    { TrackedHandJoint.ThumbTip, Pose.Landmark.LEFT_THUMB },
+    { TrackedHandJoint.ThumbMetacarpal, Pose.Landmark.LEFT_THUMB_BASE },
+    { TrackedHandJoint.ThumbProximal, Pose.Landmark.LEFT_THUMB_KNUCKLE },
+
+    { TrackedHandJoint.RingTip, Pose.Landmark.LEFT_RING },
+    { TrackedHandJoint.RingMetacarpal, Pose.Landmark.LEFT_RING_BASE },
+    { TrackedHandJoint.RingProximal, Pose.Landmark.LEFT_RING_KNUCKLE },
+
+    { TrackedHandJoint.MiddleTip, Pose.Landmark.LEFT_MIDDLE },
+    { TrackedHandJoint.MiddleMetacarpal, Pose.Landmark.LEFT_MIDDLE_BASE },
+    { TrackedHandJoint.MiddleProximal, Pose.Landmark.LEFT_MIDDLE_KNUCKLE },
+
+};
 
     void Start()
     {
@@ -32,63 +53,23 @@ public class PollHl2Hands : MonoBehaviour
             {
                 doRightHand(rightHand);
             }
+            RecognizeGesture.playerMovementRecord[0] = poseDictionary;
         }
     }
 
     void doLeftHand(IReadOnlyList<HandJointPose> leftHand)
     {
-        Debug.Log(leftHand[0].ToString());
+        foreach (int i in Enum.GetValues(typeof(TrackedHandJoint)))
+        {
+            poseDictionary[jointToLandmarkMapping[(TrackedHandJoint)i]] = leftHand[i].Position;
+        }
     }
     void doRightHand(IReadOnlyList<HandJointPose> rightHand)
     {
-        Debug.Log(rightHand[0].ToString());
-    }
-
-
-    /*
-    private void OnDisable()
-    {
-        // This component is being destroyed
-        // Instruct the Input System to disregard us for input event handling
-        CoreServices.InputSystem?.UnregisterHandler<IMixedRealitySourceStateHandler>(this);
-        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityHandJointHandler>(this);
-    }
-
-    // IMixedRealitySourceStateHandler interface
-    public void OnSourceDetected(SourceStateEventData eventData)
-    {
-        var hand = eventData.Controller as IMixedRealityHand;
-
-        // Only react to articulated hand input sources
-        if (hand != null)
+        foreach (int i in Enum.GetValues(typeof(TrackedHandJoint)))
         {
-            Debug.Log("Source detected: " + hand.ControllerHandedness);
+            poseDictionary[jointToLandmarkMapping[(TrackedHandJoint)i]] = rightHand[i].Position;
         }
     }
 
-    public void OnSourceLost(SourceStateEventData eventData)
-    {
-        var hand = eventData.Controller as IMixedRealityHand;
-
-        // Only react to articulated hand input sources
-        if (hand != null)
-        {
-            Debug.Log("Source lost: " + hand.ControllerHandedness);
-        }
-    }
-
-    Dictionary<Pose.Landmark, Vector3> poseDictionary;
-
-    public void OnHandJointsUpdated(
-                InputEventData<IDictionary<TrackedHandJoint, MixedRealityPose>> eventData)
-    {
-        MixedRealityPose handJoint;
-        if (eventData.InputData.TryGetValue(TrackedHandJoint.IndexTip, out handJoint))
-        {
-            Debug.Log("Hand Joint Palm Updated: " + handJoint.Position);
-            poseDictionary.Add(Pose.Landmark.LEFT_INDEX, handJoint.Position);
-        }
-
-        RecognizeGesture.playerMovementRecord[0] = poseDictionary;
-    }*/
 }
